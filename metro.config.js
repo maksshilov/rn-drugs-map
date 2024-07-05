bb26840fb0da5f4d12f0b0d2bce5cc8312433eb4
Max Shilov <dr.thomas.hewitt@gmail.com>
2024-07-05 07:53:19 +0300

    upgrade dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config')
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname)
+
+config.resolver.assetExts.push(
+	// Adds support for `.db` files for SQLite databases
+	'ttf'
+)
+
+module.exports = config
